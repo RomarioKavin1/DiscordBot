32a89e9c7090248fda8c2bd89c1aacc4662f0206
Romario Kavin.J.R <98270892+RomarioKavin1@users.noreply.github.com>
2022-01-24 17:57:35 +0000

    Dm functionality

--- main.py
+++ main.py
@@ -11,7 +11,17 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
-  
-  if message.content.startswith("$hello"):
-    await message.channel.send("Hello!")
+  #elif message.content.startswith("$hello"):
+    #await message.channel.send("Hello!")
+  if str(message.channel.type=="private"):
+    modmail_channel=discord.utils.get(client.get_all_channels(),name="mod_mail")
+    await modmail_channel.send("["+message.author.display_name+"]"+message.content)
+  elif str(message.channel)== "mod_mail" and message.content.startswith("<"):
+    member_object=message.mentions[0]
+
+    index= message.content.index(" ")
+    string= message.content
+    mod_message=string[index:]
+
+    await member_object.send(("["+message.author.display_name+"]"+mod_message))
 client.run(os.environ['Token'])
