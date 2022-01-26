904ddbb4c04c8bc88387fefb6f6ead47ce2423fa
Leo Franklin <94560875+LeoFranklin015@users.noreply.github.com>
2022-01-26 22:26:29 +0530

    Create buttons.py

---
+++ buttons.py
@@ -0,0 +1,54 @@
+import discord
+import os
+from discord_components import DiscordComponents, ComponentsBot, Button, SelectOption, Select
+import discord.ext.commands as commands
+client = discord.Client()
+DiscordComponents(client)
+@client.event
+async def on_ready():
+  print("we have logged in as {0.user}".format(client))
+@client.event
+async def on_message(message):
+    empty_array = []
+    modmail_channel = discord.utils.get(client.get_all_channels(), name="mod_mail")
+    mod = discord.utils.get(client.get_all_channels(), name="Mod Bot")
+  
+    if message.author == client.user:
+        return
+    if str(message.channel.type) == "private" or (str(message.channel) == "help_channel"and message.content.startswith("h:")):
+        if message.attachments != empty_array:
+            files = message.attachments
+            await modmail_channel.send("[" + message.author.display_name + "]")
+
+            for file in files:
+                await modmail_channel.send(file.url)
+        elif (str(message.channel) == "help_channel"and message.content.startswith("h:")):
+            index = message.content.index(":")+1
+            string = message.content
+            help_message = string[index:]
+            await modmail_channel.send("[" + message.author.display_name + "] " + help_message)
+        else:
+          await message.channel.send("hello", components = [
+        [Button(label="Hi", style="3", emoji = "🥴", custom_id="button1"), Button(label="Bye", style="4", emoji = "😔", custom_id="button2")]
+        ])
+          interaction = await client.wait_for("button_click", check = lambda i: i.custom_id == "button1")
+          a=await interaction.send(content = "Button clicked!", ephemeral=True)
+          
+          await modmail_channel.send("[" + message.author.display_name + "] " + message.content)
+          
+    elif str(message.channel) == "mod_mail" and message.content.startswith("<"):
+        member_object = message.mentions[0]
+        if message.attachments != empty_array:
+            files = message.attachments
+            await member_object.send("[" + message.author.display_name + "]")
+
+            for file in files:
+                await member_object.send(file.url)
+        else:
+            index = message.content.index(" ")
+            string = message.content
+            mod_message = string[index:]
+            await member_object.send("[" + message.author.display_name + "]" + mod_message)
+
+
+client.run('OTM1OTIxNTAwMTMxODQ0MTQ2.YfFq0g.Fw70T0O8INKCYkAOfifc8OWzenE')
